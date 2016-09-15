af6e6e9ddb4bef4f4d8f97cdb5b8769c760088de
Zachery Grace <zacherygrace@Zacharys-MacBook-Pro.local>
2016-09-14 22:03:10 -1000

    problem 5

--- objects.js
+++ objects.js
@@ -89,6 +89,69 @@ console.log (buildPerson(plainPerson, "Zach", 24));
             ...
  */
 
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
+
+function printProcessedOrders(orders){
+    for (var i = 0; i < orders.length; i++){
+        console.log ("id: ", orders[i].id);
+        console.log ("purchase date: ", orders[i].date);
+        console.log ("purchase total: ", orders[i].total);
+    }
+    return orders[i];
+}
+
+console.log (printProcessedOrders(arrayOfObjects));
 
 /*
     # Addition with an object
@@ -101,7 +164,18 @@ console.log (buildPerson(plainPerson, "Zach", 24));
         Go ahead and create some more objects and pass them to this function. Have fun with it.
 */
 
+var sumObj = {
+    a: 2,
+    b: 10,
+    result: undefined
+};
 
+function objectAddition (object1){
+    object1.result = object1.a + object1.b;
+        return object1;
+}
+
+console.log (objectAddition(sumObj));
 /*
     # Print sum function and add as new key-value
         Declare a new function named printObj and a single parameter which will be the object from the challenge just above. Within this function you are to print to the screen a message of the operation performed. For Example:
